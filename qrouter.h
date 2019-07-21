43717e73904e80281f23ea82a576b78cd2390352
Roman-Parise <pariseroman@gmail.com>
2019-07-21 10:31:23 -0700

    Manually defined type for FreeBSD.
    Change fixes a compiler error.

--- qrouter.h
+++ qrouter.h
@@ -40,7 +40,7 @@ typedef unsigned long  u_long;
 #endif /* _SYS_TYPES_H */
 
 /* Compare functions aren't defined in the Mac's standard library */
-#if defined(__APPLE__)
+#if defined(__APPLE__) || defined(__FreeBSD__)
 typedef int (*__compar_fn_t)(const void*, const void*);
 #endif
 
