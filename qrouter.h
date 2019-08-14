b95a124702f7bd625d26954a1412a38802ed14b8
Tim Edwards <tim@opencircuitdesign.com>
2019-08-14 17:14:54 -0400

    Added extensions to qrouter to handle 90 degree rotations in DEF components, which were previously not handled.

--- qrouter.h
+++ qrouter.h
@@ -63,8 +63,9 @@ typedef int (*__compar_fn_t)(const void*, const void*);
 // define possible gate orientations
 
 #define MNONE	0
-#define MX	1
-#define MY	2
+#define MX	1   // flipped in X
+#define MY	2   // flipped in Y
+#define R90	4   // rotated clockwise 90 degrees
 
 // define search directions
 
